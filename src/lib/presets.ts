eea5062ebb95bc508adf08e795cee61776b4fe3e
tjhuggins01-dot <tjhuggins01@gmail.com>
2026-03-03 21:30:36 +0000

    Update presets.ts

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -167,8 +167,8 @@ const VS_OPEN_DEFAULTS_V2: Record<string, { call: string; threeBet: string }> = 
     call: '22,33,44,55,66,77,88,A2s,A6s,A7s,A8s,A9s,ATs,AJs,AJo,ATo,K8s,K9s,KTs,KJo,KQo,Q9s,QTs,QJo,QTo,J9s,JTo,T8s,T9o,98s,97s,87s,86s,76s,75s,65s,64s,54s,43s,53s',
   },
   BB_vs_BTN: {
-    threeBet: '88,99,TT,JJ,QQ,KK,AA,AKs,AKo,AQs,AQo,A5s,A4s,A3s,A2s,KQs,KJs,QJs,JTs,T9s,98s,87s,76s',
-    call: '22,33,44,55,66,77,A6s,A7s,A8s,A9s,ATs,AJs,AJo,ATo,A9o,K7s,K8s,K9s,KTs,KJo,KQo,Q8s,Q9s,QTs,QJo,QTo,J8s,J9s,JTo,T8s,T9o,97s,86s,85s,75s,65s,64s,54s,53s,43s',
+    threeBet: '66,77,88,99,TT,JJ,QQ,KK,AA,AKs,AKo,AQs,AQo,A5s,A4s,A3s,A2s,KQs,KJs,QJs,JTs,T9s,98s,87s,76s',
+    call: '22,33,44,55,A6s,A7s,A8s,A9s,ATs,AJs,AJo,ATo,A9o,K7s,K8s,K9s,KTs,KJo,KQo,Q8s,Q9s,QTs,QJo,QTo,J8s,J9s,JTo,T8s,T9o,97s,86s,85s,75s,65s,64s,54s,53s,43s',
   },
   BB_vs_SB: {
     threeBet: '88,99,TT,JJ,QQ,KK,AA,AKs,AKo,AQs,AQo,A5s,A4s,A3s,KQs,KJs,QJs',
